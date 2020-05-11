063f6a4daec973cc6d438ec08740848b3d5da7f5
TianYi Li <tli12316@terpmail.umd.edu>
2020-05-11 16:06:49 -0400

    update

--- interface.py
+++ interface.py
@@ -10,18 +10,22 @@ class Interface:
         global root
         root = Tk()
         root.title("Expense Tracker")
-        mybutton1 = Button(root,text="Click to start!", command = self.myClick,width = 25, height = 5)
+        mybutton1 = Button(root,text="Click to start!", command = self.mybutton,width = 25, height = 5)
         mybutton1.pack()
+        root.mainloop()
 
 
-    def myClick(self):
+    def mybutton(self):
         """Create a button to let user interact with"""
+        mybutton2 = Button(root,text="Company and Acount type", command = self.mycompany,width = 25, height = 5)
+        mybutton2.pack()
+     
 
+    def mycompany(self):
         mylabel = Label(root,text = "Your Daily Payment Is: ")
         mylabel.pack()
-
         
 
 if __name__ == '__main__': 
     myInterface = Interface()
-    root.mainloop()
+    
